658af942b067103b425091bfa6778d5817760e35
Robson Sagaz <robsonluissagaz@gmail.com>
2025-01-02 11:13:48 -0300

    att

--- main.py
+++ main.py
@@ -159,42 +159,6 @@ class RemoverVigilanteScreen(Screen):
         content.add_widget(buttons)
         popup = Popup(title="Confirmação", content=content, size_hint=(0.6, 0.4), auto_dismiss=False)
         popup.open()
-
-
-    def remover_vigilante(self):
-        nome_vigilante = self.ids.nome_vigilante_remover.text.strip().upper()
-        if not nome_vigilante:
-            self.show_popup("Erro", "Por favor, insira o nome completo do vigilante.")
-            return
-        try:
-            query = "SELECT nome, cargo FROM vigilantes WHERE nome = %s"
-            self.cursor.execute(query, (nome_vigilante,))
-            dados = self.cursor.fetchone()
-            if not dados:
-                self.show_popup("Aviso", f"Usuário '{nome_vigilante}' não encontrado.")
-                return
-            nome_do_vigilante, cargo_do_vigilante = dados
-            if cargo_do_vigilante == "SUPERVISOR":
-                self.show_popup("Erro", f"Você não tem permissão para remover o usuário '{nome_do_vigilante}'")
-                return
-            content = BoxLayout(orientation='vertical', spacing=10, padding=10)
-            content.add_widget(Label(text=f"Tem certeza que deseja remover o usuário '{nome_vigilante}'?"))
-            btn_layout = BoxLayout(orientation='horizontal', spacing=10)
-            btn_confirmar = Button(text="Confirmar", size_hint=(0.5, None), height=40)
-            btn_cancelar = Button(text="Cancelar", size_hint=(0.5, None), height=40)
-            popup = Popup(
-                title="Confirmação",
-                content=content,
-                size_hint=(0.6, 0.4),
-                auto_dismiss=False,)
-            btn_confirmar.bind(on_release=lambda x: self.confirmar_remocao(popup, nome_vigilante))
-            btn_cancelar.bind(on_release=popup.dismiss)
-            btn_layout.add_widget(btn_confirmar)
-            btn_layout.add_widget(btn_cancelar)
-            content.add_widget(btn_layout)
-            popup.open()
-        except mysql.connector.Error as e:
-            self.show_popup("Erro", f"Erro ao acessar o banco de dados: {str(e)}")
     
 
     def confirmar_remocao(self, popup, nome_vigilante):
@@ -230,10 +194,6 @@ class VigilanteScreen(Screen):
     def atualizar_label(self):
         global nome_usuario_letreiro
         self.ids.letreiro_vigilante.text = f'BEM VINDO {nome_usuario_letreiro}'
-    pass
-
-
-class ErroSenhaUsuarioScreen(Screen):
     pass
 
 
